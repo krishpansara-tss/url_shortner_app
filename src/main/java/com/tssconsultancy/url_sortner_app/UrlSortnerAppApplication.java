0584df548cdd3f77b7314403405aaf85c6266ef4
Krish Pansara <krish.pansara@tssconsultancy.com>
2026-08-08 18:37:33 +0530

    first commit

---
+++ src/main/java/com/tssconsultancy/url_sortner_app/UrlSortnerAppApplication.java
@@ -0,0 +1,13 @@
+package com.tssconsultancy.url_sortner_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSortnerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlSortnerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tssconsultancy/url_sortner_app/UrlSortnerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tssconsultancy.url_sortner_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlSortnerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
